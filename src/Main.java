4a808c6859f1b1bcd73ce04ff8ed7c75e96d3ab8
Arpit Sahu <aptsahu2@gmail.com>
2023-07-06 10:55:47 +0530

    Added Initial files

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Main m1 = new Main();
+        m1.amnaHello();
+        m1.giteshHello();
+        m1.sonalHello();
+        m1.swapnilHello();
+    }
+    public void swapnilHello(){
+        System.out.println("Hello Swapnil");
+    }
+    public void giteshHello(){
+        System.out.println("Hello Gitesh");
+    }
+    public void amnaHello(){
+        System.out.println("Hello Amna");
+    }
+    public void sonalHello(){
+        System.out.println("Hello Sonal");
+    }
+}
